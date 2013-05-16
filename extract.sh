b632aee7b429ce8aca707e31c8ba18e7962defd3
Vitalii Tereshchuk <xvoland@gmail.com>
2013-05-17 00:52:43 +0300

    init

---
+++ extract.sh
@@ -0,0 +1,29 @@
+function extract {
+ if [ -z "$1" ]; then
+    echo "Usage: extract <path/file_name>.<zip|rar|bz2|gz|tar|tbz2|tgz|Z|7z|xz|ex|tar.bz2|tar.gz|tar.xz>"
+ else
+    if [ -f $1 ] ; then
+        NAME=${1%.*}
+        mkdir $NAME && cd $NAME
+        case $1 in
+          *.tar.bz2)   tar xvjf ../$1    ;;
+          *.tar.gz)    tar xvzf ../$1    ;;
+          *.tar.xz)    tar xvJf ../$1    ;;
+          *.bz2)       bunzip2 ../$1     ;;
+          *.rar)       unrar x ../$1     ;;
+          *.gz)        gunzip ../$1      ;;
+          *.tar)       tar xvf ../$1     ;;
+          *.tbz2)      tar xvjf ../$1    ;;
+          *.tgz)       tar xvzf ../$1    ;;
+          *.zip)       unzip ../$1       ;;
+          *.Z)         uncompress ../$1  ;;
+          *.7z)        7z x ../$1        ;;
+          *.xz)        unxz ../$1        ;;
+          *.exe)       cabextract ../$1  ;;
+          *)           echo "\'$1': unknown archive method" ;;
+        esac
+    else
+        echo "\'$1' file does not exist"
+    fi
+fi
+}
