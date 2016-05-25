9a3d5b248adac919fe46e48156a645487fe94475
Xavier Pigeon <xavier.pigeon@prestataire.ing.fr>
2016-05-25 17:52:22 +0200

    Convert the file name to lower case.

--- extract.sh
+++ extract.sh
@@ -7,9 +7,8 @@ function extract {
     echo "Usage: extract <path/file_name>.<zip|rar|bz2|gz|tar|tbz2|tgz|Z|7z|xz|ex|tar.bz2|tar.gz|tar.xz>"
  else
     if [ -f "$1" ] ; then
-        NAME=${1%.*}
-        #mkdir $NAME && cd $NAME
-        case "$1" in
+        local nameInLowerCase=`echo "$1" | awk '{print tolower($0)}'`
+        case "$nameInLowerCase" in
           *.tar.bz2)   tar xvjf ./"$1"    ;;
           *.tar.gz)    tar xvzf ./"$1"    ;;
           *.tar.xz)    tar xvJf ./"$1"    ;;
