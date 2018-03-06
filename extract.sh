666ec5a310fdd90b9408378f3993d5e4bfda3061
monyarm <monyarm@gmail.com>
2018-03-07 01:17:47 +0200

    Case Insensitive extentions

--- extract.sh
+++ extract.sh
@@ -36,22 +36,42 @@ function extract {
     do
       if [ -f "$n" ] ; then
           case "${n%,}" in
-            *.tar.bz2|*.tar.gz|*.tar.xz|*.tbz2|*.tgz|*.txz|*.tar) 
-                         tar xvf "$n"       ;;
-            *.lzma)      unlzma ./"$n"      ;;
-            *.bz2)       bunzip2 ./"$n"     ;;
-            *.rar)       unrar x -ad ./"$n" ;;
-            *.gz)        gunzip ./"$n"      ;;
-            *.zip)       unzip ./"$n"       ;;
-            *.z)         uncompress ./"$n"  ;;
-            *.7z|*.arj|*.cab|*.chm|*.deb|*.dmg|*.iso|*.lzh|*.msi|*.rpm|*.udf|*.wim|*.xar|*.apk)
-                         7z x ./"$n"        ;;
-            *.xz)        unxz ./"$n"        ;;
-            *.exe)       cabextract ./"$n"  ;;
-            *.zpaq)      zpaq x ./"$n"      ;;
-            *.ace)       unace e ./"$n"     ;;
-            *.arc)       arc e ./"$n"       ;;
-            *.cso)       ciso 0 ./"$n" ./"$n.iso" && extract $n.iso && \rm -f $n ;;
+            *.tar.bz2|*.Tar.bz2|*.Tar.Bz2|*.tar.Bz2|*.tar.BZ2|*.TAR.bz2|*.TAR.BZ2|\
+            *.tar.gz|*.Tar.gz|*.Tar.Gz|*.tar.Gz|*.tar.GZ|*.TAR.gz|*.TAR.GZ|\
+            *.tar.xz|*.Tar.xz|*.Tar.Xz|*.tar.Xz|*.tar.XZ|*.TAR.xz|*.TAR.XZ|\
+            *.tbz2|*.Tbz2|*.TBZ2|\
+            *.tgz|*.Tgz|*.TGZ|\
+            *.txz|*.Txz|*.TXZ|\
+            *.tar|*.Tar|*.TAR) 
+                                       tar xvf "$n"       ;;
+            *.lzma|*.Lzma|*.LZMA)      unlzma ./"$n"      ;;
+            *.bz2|*.Bz2|*.BZ2)         bunzip2 ./"$n"     ;;
+            *.rar|*.Rar|*.RAR)         unrar x -ad ./"$n" ;;
+            *.gz|*.Gz|*.GZ)            gunzip ./"$n"      ;;
+            *.zip|*.Zip|*.ZIP)         unzip ./"$n"       ;;
+            *.z|*.Z)                   uncompress ./"$n"  ;;
+            *.7z|*.7Z|\
+            *.apk|*.Apk|*.APK|\
+            *.arj|*.Arj|*.ARJ|\
+            *.cab|*.Cab|*.CAB|\
+            *.chm|*.Chm|*.CHM|\
+            *.deb|*.Deb|*.DEB|\
+            *.dmg|*.Dmg|*.DMG|\
+            *.iso|*.Iso|*.ISO|\
+            *.lzh|*.Lzh|*.LZH|\
+            *.msi|*.Msi|*.MSI|\
+            *.rpm|*.Rpm|*.RPM|\
+            *.udf|*.Udf|*.UDF|\
+            *.wim|*.Wim|*.WIM|\
+            *.xar|*.Xar|*.XAR)
+                                       7z x ./"$n"        ;;
+            *.xz|*.Xz|*.XZ)            unxz ./"$n"        ;;
+            *.exe|*.Exe|*.EXE)         cabextract ./"$n"  ;;
+            *.zpaq|*.Zpaq|*.ZPAQ)      zpaq x ./"$n"      ;;
+            *.ace|*.Ace|*.ACE)         unace e ./"$n"     ;;
+            *.arc|*.Arc|*.ARC)         arc e ./"$n"       ;;
+            *.cso|*.Cso|*.CSO)       ciso 0 ./"$n" ./"$n.iso" && \
+                              extract $n.iso && \rm -f $n ;;
             *)
                          echo "extract: '$n' - unknown archive method"
                          return 1
