34a35c33b4a34916e9955d2d725831e7882d70f5
Stavros Ntentos <133706+stdedos@users.noreply.github.com>
2020-12-23 03:40:56 +0200

    Handle extractions inside a directory
    * Create a temp directory, hardlink the extracted file inside,
      and then extract it.
    * Add a force-extract-in-dir flag (`-D`)
    * Add `*.docx|*.xlsx` extraction
    * Convert `ar` extraction to use the new method
    
    Signed-off-by: Stavros Ntentos <133706+stdedos@users.noreply.github.com>

--- extract.sh
+++ extract.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 # function Extract for common file formats
 
+in_temp() {
+  set -Eeuo pipefail
+
+  in_dir="$(mktemp -dp . "${n}-XXXXX")"
+  >&2 echo "Copying (hardlink) '$n' in '${in_dir}/$n'"
+
+  cp -l "$1" "${in_dir}/$n"
+  echo "${in_dir}"
+}
+
 function extract {
   local EXIT_CODE=0
-  local TMPDIR=
   local VERBOSE=1
   local KEEP=0
+  local FORCE_DIR=1
+  local in_dir
   local -a VERBOSE_FLAG
   local -a KEEP_FLAG
 
@@ -26,13 +37,14 @@ function extract {
   for n ; do
     VERBOSE_FLAG=()
     KEEP_FLAG=()
-    TMPDIR=
+    in_dir=
 
     case $n in
-      -v) VERBOSE=0 ; continue ;;
-      -V) VERBOSE=1 ; continue ;;
-      -k) KEEP=0    ; continue ;;
-      -K) KEEP=1    ; continue ;;
+      -v) VERBOSE=0     ; continue ;;
+      -V) VERBOSE=1     ; continue ;;
+      -k) KEEP=0        ; continue ;;
+      -K) KEEP=1        ; continue ;;
+      -D) FORCE_DIR=0   ; continue ;;
       *)  ;;
     esac
 
@@ -45,6 +57,16 @@ function extract {
       echo "===> Extracting: $n <===" >&2
     fi
 
+    if [ "${FORCE_DIR}" -eq 0 ] ; then
+      if ! in_dir="$(in_temp "$n")" ; then
+        echo "Failed to prepare '$n' for extraction!" >&2
+      fi
+      if ! (cd "${in_dir}" && extract "$n") ; then
+        EXIT_CODE="$((EXIT_CODE + 1))"
+      fi
+      continue
+    fi
+
     case "${n%,}" in
       *.cbt|*.tar.bz2|*.tar.gz|*.tar.xz|*.tbz2|*.tgz|*.txz|*.tar)
                               test "${VERBOSE}" -eq 0 && VERBOSE_FLAG=(v)
@@ -55,10 +77,20 @@ function extract {
       *.cbr|*.rar)            unrar x -ad "$n" ;;
       *.gz)                   test "${KEEP}" -eq 0 && KEEP_FLAG=(-k)
                               gunzip "${KEEP_FLAG[@]}" "$n"      ;;
+      *.docx|*.xlsx)
+        if ! in_dir="$(in_temp "$n")" ; then
+          echo "Failed to prepare '$n' for extraction!" >&2
+        fi
+      ;&
       *.cbz|*.epub|*.zip|*.whl)
         # We test quiet instead
         test "${VERBOSE}" -eq 1 && VERBOSE_FLAG=(-q)
-        unzip "${VERBOSE_FLAG[@]}" "$n"
+        (
+          if [ -n "${in_dir}" ] ; then
+            cd "${in_dir}" || exit $?
+          fi
+          unzip "${VERBOSE_FLAG[@]}" "$n"
+        )
       ;;
       *.z)                    uncompress "$n"  ;;
       # ar is better for *.deb files
@@ -67,20 +99,10 @@ function extract {
 
         # ar extracts "here", and debfiles have
         # multiple and similarly-named files
-        TMPDIR="$(basename "$n")"
-        (
-          set -Eeuo pipefail
-          mkdir "${TMPDIR}"
-          cd "${TMPDIR}"
-          case "$n" in
-            /*) ar "${VERBOSE_FLAG[*]}"x    "$n" ;;
-             *) ar "${VERBOSE_FLAG[*]}"x ../"$n" ;;
-          esac
-        )
-        # shellcheck disable=SC2181
-        if [ "$?" -ne 0 ] ; then
-          echo "Failed to extract '$n' in '$(realpath "${TMPDIR}")'" >&2
+        if ! in_dir="$(in_temp "$n")" ; then
+          echo "Failed to prepare '$n' for extraction!" >&2
         fi
+        (cd "${in_dir}" && ar "${VERBOSE_FLAG[*]}"x "$n")
       ;;
       *.7z|*.arj|*.cab|*.cb7|*.chm|*.deb|*.dmg|*.iso|*.lzh|*.msi|*.pkg|*.rpm|*.udf|*.wim|*.xar)
                               7z x "$n"        ;;
@@ -93,11 +115,14 @@ function extract {
       *.cso)                  ciso 0 "$n" "$n.iso" && extract "$n.iso" && command rm -f "$n" ;;
       *)
         echo "extract: '$n' - unknown archive method"
-
-        EXIT_CODE="$((EXIT_CODE + 1))"
-        continue
+        (exit 1)
       ;;
       esac
+
+      # shellcheck disable=SC2181
+      if [ "$?" -ne 0 ] ; then
+        EXIT_CODE="$((EXIT_CODE + 1))"
+      fi
   done
 
   return "${EXIT_CODE}"
