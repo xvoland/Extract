8fc4c9ca36147bf024d0048d0b7c3652033e17c3
xVoLAnD <xvoland@gmail.com>
2025-03-09 02:20:26 +0200

    no message

--- extract.sh
+++ extract.sh
@@ -17,17 +17,19 @@
 # Web:    https://dotoca.net
 # Github: https://github.com/xvoland/Extract/blob/master/extract.sh
 
-set -e  # abort execution on errors
 
 SAVEIFS=$IFS
 IFS=$' \t\n'
 
 function extract {
- if [ $# -eq 0 ]; then
-    # display usage if no parameters given
-    echo "Usage: extract <path/file_name>.<zip|rar|bz2|gz|tar|tbz2|tgz|Z|7z|xz|ex|tar.bz2|tar.gz|tar.xz|.zlib|.cso|.zst>"
-    echo "       extract <path/file_name_1.ext> [path/file_name_2.ext] [path/file_name_3.ext]"
- fi
+    set -e  # abort execution on errors
+
+    if [ $# -eq 0 ]; then
+        # display usage if no parameters given
+        echo "Usage: extract <path/file_name>.<zip|rar|bz2|gz|tar|tbz2|tgz|Z|7z|xz|ex|tar.bz2|tar.gz|tar.xz|.zlib|.cso|.zst>"
+        echo "       extract <path/file_name_1.ext> [path/file_name_2.ext] [path/file_name_3.ext]"
+    fi
+
     for n in "$@"; do
         if [ ! -f "$n" ]; then
             echo "'$n' - file doesn't exist"
@@ -43,31 +45,31 @@ function extract {
                     tar xvf "$n"
                 fi
                 ;;
-          *.lzma)       unlzma ./"$n"      ;;
-          *.bz2)        bunzip2 ./"$n"     ;;
-          *.cbr|*.rar)  unrar x -ad ./"$n" ;;
-          *.gz)         gunzip ./"$n"      ;;
-          *.cbz|*.epub|*.zip) unzip ./"$n" ;;
-          *.z)          uncompress ./"$n"  ;;
-          *.7z|*.apk|*.arj|*.cab|*.cb7|*.chm|*.deb|*.iso|*.lzh|*.msi|*.pkg|*.rpm|*.udf|*.wim|*.xar|*.vhd)
+        *.lzma)        unlzma ./"$n"      ;;
+        *.bz2)         bunzip2 ./"$n"     ;;
+        *.cbr|*.rar)   unrar x -ad ./"$n" ;;
+        *.gz)          gunzip ./"$n"      ;;
+        *.cbz|*.epub|*.zip) unzip ./"$n" ;;
+        *.z)           uncompress ./"$n"  ;;
+        *.7z|*.apk|*.arj|*.cab|*.cb7|*.chm|*.deb|*.iso|*.lzh|*.msi|*.pkg|*.rpm|*.udf|*.wim|*.xar|*.vhd)
                         7z x ./"$n"        ;;
-          *.xz)         unxz ./"$n"        ;;
-          *.exe)        cabextract ./"$n"  ;;
-          *.cpio)       cpio -id < ./"$n"  ;;
-          *.cba|*.ace)  unace x ./"$n"     ;;
-          *.zpaq)       zpaq x ./"$n"      ;;
-          *.arc)        arc e ./"$n"       ;;
-          *.cso)        ciso 0 ./"$n" ./"$n.iso" && \
+        *.xz)           unxz ./"$n"        ;;
+        *.exe)          cabextract ./"$n"  ;;
+        *.cpio)         cpio -id < ./"$n"  ;;
+        *.cba|*.ace)    unace x ./"$n"     ;;
+        *.zpaq)         zpaq x ./"$n"      ;;
+        *.arc)          arc e ./"$n"       ;;
+        *.cso)          ciso 0 ./"$n" ./"$n.iso" && \
                         extract "$n.iso" && \rm -f "$n" ;;
-          *.zlib)       zlib-flate -uncompress < ./"$n" > ./"$n.tmp" && \
+        *.zlib)         zlib-flate -uncompress < ./"$n" > ./"$n.tmp" && \
                         mv ./"$n.tmp" ./"${n%.*zlib}" && rm -f "$n"   ;;
-          *.dmg)
+        *.dmg)
                         mnt_dir=$(mktemp -d)
                         hdiutil mount "$n" -mountpoint "$mnt_dir"
                         echo "Mounted at: $mnt_dir" ;;
-          *.tar.zst)    tar -I zstd -xvf ./"$n"  ;;
-          *.zst)        zstd -d ./"$n"  ;;
-          *)
+        *.tar.zst)      tar -I zstd -xvf ./"$n"  ;;
+        *.zst)          zstd -d ./"$n"  ;;
+        *)
                         echo "extract: '$n' - unknown archive method"
                         return 1
                         ;;
